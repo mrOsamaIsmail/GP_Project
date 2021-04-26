57d7c52a0f46a886234ff2ddfa86a880460f57c2
osama <osamaxnasrala@gmail.com>
2021-04-26 14:44:48 +0200

    new files

---
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "AddDoctor", id = UrlParameter.Optional }
+            );
+            routes.MapRoute
+                (
+                name: "Sponsor",
+                url: "{controller}/{action}/{id}",
+                defaults : new { controller = "Sponsor",action = "SponsorLogin",id = UrlParameter.Optional }
+            );
+        }
+    }
+}
